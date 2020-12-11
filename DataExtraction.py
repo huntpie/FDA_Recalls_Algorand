e7dbac9d5fe1529b05156e6e6a5c29100ef2744c
Pierce Hunt <huntpie@gmail.com>
2020-12-10 22:31:37 -0500

    Split the json to fit notefield length for Smart Contract

--- DataExtraction.py
+++ DataExtraction.py
@@ -1,17 +1,25 @@
+import base64
 import requests
 import json
-import csv
+from itertools import islice
 
-url = "https://api.fda.gov/food/enforcement.json?search=report_date:[20201201+TO+20201202]&limit=100"
+url = "https://api.fda.gov/food/enforcement.json?search=report_date:[20201201+TO+20201202]&limit=1"
 
 response = requests.get(url)
 
 data = response.text
 
 parsed = json.loads(data)
-results_country = parsed["results"][0]["country"]
-results_city = parsed["results"][0]["city"]
+results_dict = parsed["results"][0]
+
+inc = iter(results_dict.items())
+results_split_1 = dict(islice(inc, len(results_dict) // 2))
+results_split_2 = dict(inc)
+
+results_json_1 = json.dumps(results_split_1)
+
+print(results_json_1)
 
 
-with open('FDA20201201to20201202.csv', 'w+') as f:
-    f.write("Country: " + results_country + ", City: " + results_city)
+# with open('FDA20201201to20201202.csv', 'w+') as f:
+# f.write("Country: " + results_country + ", City: " + results_city)
---
+++ JasonPythonAlgoTutorial2.py
@@ -0,0 +1,74 @@
+import base64
+import json
+import time
+
+import DataExtraction
+from algosdk import algod, mnemonic, transaction
+
+
+def wait_for_confirmation(algod_client, txid):
+    last_round = algod_client.status().get('lastRound')
+    while True:
+        txinfo = algod_client.pending_transaction_info(txid)
+        if txinfo.get('round') and txinfo.get('round') > 0:
+            print("Transaction {} confirmed in round {}.".format(
+                txid, txinfo.get('round')))
+            break
+        else:
+            print("Waiting for confirmation...")
+            last_round += 1
+            algod_client.status_after_block(last_round)
+
+
+def send_note():
+    algod_address = "http://localhost:4001"
+    algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    algod_client = algod.AlgodClient(algod_token, algod_address)
+
+    passphrase = "neglect struggle demand unfold knee salute mechanic enlist tray mind outside topple garage antique invest narrow gesture cat mouse solid slight myself favorite about raw"
+
+    private_key = mnemonic.to_private_key(passphrase)
+    my_address = mnemonic.to_public_key(passphrase)
+    print("My address: {}".format(my_address))
+
+    params = algod_client.suggested_params()
+    note = DataExtraction.results_json_1.encode()
+    receiver = "GD64YIY3TWGDMCNPP553DZPPR6LDUSFQOIJVFDPPXWEG3FVOJCCDBBHU5A"
+
+    data = {
+        "sender": my_address,
+        "receiver": receiver,
+        "fee": params.get('minFee'),
+        "flat_fee": True,
+        "amt": 0,
+        "first": params.get('lastRound'),
+        "last": params.get('lastRound') + 1000,
+        "note": note,
+        "gen": params.get('genesisID'),
+        "gh": params.get('genesishashb64')
+    }
+
+    txn = transaction.PaymentTxn(**data)
+    signed_txn = txn.sign(private_key)
+    txid = signed_txn.transaction.get_txid()
+    print("Signed transaction with txID: {}".format(txid))
+
+    algod_client.send_transaction(signed_txn)
+
+    # wait for confirmation
+    wait_for_confirmation(algod_client, txid)
+
+    try:
+        confirmed_txn = algod_client.transaction_info(my_address, txid)
+    except Exception as err:
+        print(err)
+    print("Transaction information: {}".format(
+        json.dumps(confirmed_txn, indent=4)))
+    print("Decoded note: {}".format(base64.b64decode(
+        confirmed_txn.get('noteb64')).decode()))
+    person_dict = json.loads(base64.b64decode(
+        confirmed_txn.get('noteb64')).decode())
+    # print(person_dict['firstName'])
+
+
+send_note()
